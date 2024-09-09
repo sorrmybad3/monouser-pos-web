3819c47980adf1c9d7f9d261051612e3cfd14e4a
sorrmybad3 <smbad3@outlook.com>
2024-09-09 12:16:58 -0500

    icons installed, login and basic dashboard navigation implementation.

---
+++ src/components/DashboardOutlet/Orders.outlet.tsx
@@ -0,0 +1,10 @@
+import { Text, Title } from "@mantine/core";
+
+export function OrdersOutlet() {
+  return (
+    <>
+      <Title>Orders Outlet</Title>
+      <Text>Manage and view your orders here.</Text>
+    </> 
+  );
+}
---
+++ src/components/DashboardOutlet/Products.outlet.tsx
@@ -0,0 +1,10 @@
+import { Text, Title } from "@mantine/core";
+
+export function ProductsOutlet() {
+  return (
+    <>
+      <Title>Products Outlet</Title>
+      <Text>Manage and view your products here.</Text>
+    </> 
+  );
+}
