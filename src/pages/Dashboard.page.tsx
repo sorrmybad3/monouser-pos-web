a464e239e186668d03f687984bac22e865ffe6e0
sorrmybad3 <smbad3@outlook.com>
2024-09-10 14:04:15 -0500

    Renamed Navigation Bar file.

--- src/pages/Dashboard.page.tsx
+++ src/pages/Dashboard.page.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import { Grid } from '@mantine/core';
-import { NavigationBar } from '@/components/Navigation/Navigation';
+import { NavigationBar } from '@/components/Navigation/NavigationBar';
 
 export function DashboardPage() {
   return (
