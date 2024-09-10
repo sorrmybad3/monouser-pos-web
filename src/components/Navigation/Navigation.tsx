e472c3879927e4521b29876710f9f0550deb0961
sorrmybad3 <smbad3@outlook.com>
2024-09-10 14:02:25 -0500

    Removed href from route links.

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -14,32 +14,27 @@ export function Navigation() {
   return (
     <Container p={4} pos="sticky" top={0} size="100%">
       <NavLink
-        href="#required-for-focus"
         label="POS"
         leftSection={<IconBuildingStore size="1rem" stroke={1.5} />}
         onClick={() => navigate('/dashboard/pos')}
       />
       <NavLink
-        href="#required-for-focus"
         label="Inventory"
         leftSection={<IconBuildingWarehouse size="1rem" stroke={1.5} />}
         childrenOffset={28}
       >
         <NavLink
-          href="#required-for-focus"
           label="Products"
           leftSection={<IconBadgeTm size="1rem" stroke={1.5} />}
           onClick={() => navigate('/dashboard/products')}
         />
       </NavLink>
       <NavLink
-        href="#required-for-focus"
         label="Sales"
         leftSection={<IconReportMoney size="1rem" stroke={1.5} />}
         childrenOffset={28}
       >
         <NavLink
-          href="#required-for-focus"
           label="Orders"
           leftSection={<IconReceipt size="1rem" stroke={1.5} />}
           onClick={() => navigate('/dashboard/orders')}
